2fe85d0a59a46b1a5fd44dceb1dd00589f4bd3a0
Christopher Webb <cdw2146@columbia.edu>
2024-11-11 17:27:39 -0500

    update models

--- COMSW4111/data_models/bank_account.py
+++ COMSW4111/data_models/bank_account.py
@@ -4,6 +4,6 @@ from COMSW4111.data_models import db
 
 class BankAccount(db.Model):
     __tablename__ = 'pr_bankaccount'
-    account_id = db.Column(db.String(50), db.ForeignKey('account.account_id'))
+    account_id = db.Column(db.String(50), db.ForeignKey('pr_account.account_id'))
     bank_acc_num = db.Column(db.String(30), primary_key=True)
     routing_num = db.Column(db.String(50))
